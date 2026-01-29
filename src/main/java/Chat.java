119deab5175dacc04b6398a2afd5040c2b9fdcff
yqzzy <yiqian511@gmail.com>
2026-01-29 11:14:36 +0800

    Changed name to chat

---
+++ src/main/java/Chat.java
@@ -0,0 +1,12 @@
+public class Chat {
+    public static void main(String[] args) {
+        String logo = " CCCCC  H   H   AAAA   TTTTT\n" +
+                "C       H   H  A    A    T  \n" +
+                "C       HHHHH  AAAAAA    T  \n" +
+                "C       H   H  A    A    T  \n" +
+                " CCCCC  H   H  A    A    T  ";
+        System.out.println("Hello I'm \n" + logo);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
