b6bc741124ad9eaee4751373b04028fb29c828c6
yqzzy <yiqian511@gmail.com>
2026-01-30 18:14:53 +0800

    Add echo

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Chat {
     public static void main(String[] args) {
         String logo = " CCCCC  H   H   AAAA   TTTTT\n" +
@@ -7,6 +9,13 @@ public class Chat {
                 " CCCCC  H   H  A    A    T  ";
         System.out.println("Hello I'm \n" + logo);
         System.out.println("What can I do for you?\n");
+        String input;
+        Scanner in = new Scanner(System.in);
+        do {
+            input =  in.nextLine();
+            System.out.println(input);
+            System.out.println("----------------------------------------");
+        } while (!"bye".equals(input));
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
