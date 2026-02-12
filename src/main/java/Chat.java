d560cfd616870d869d024b90af25c83b978b7ad3
yqzzy <yiqian511@gmail.com>
2026-02-12 17:23:15 +0800

    Extracted methods from main(): printWelcome(), handleCommand(), printList(), findTaskByDescription(), markOrUnmarkTask(), createAndAddTask(), removed redundant size variable, improved input validation

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -2,89 +2,128 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Chat {
+    private static final String DIVIDER = "----------------------------------------";
+    private static final String LOGO = """
+             CCCCC  H   H   AAAA   TTTTT
+            C       H   H  A    A    T
+            C       HHHHH  AAAAAA    T
+            C       H   H  A    A    T
+             CCCCC  H   H  A    A    T""";
+    private static boolean isActive = true;
+
     public static void main(String[] args) {
-        String logo = """
-                 CCCCC  H   H   AAAA   TTTTT
-                C       H   H  A    A    T \s
-                C       HHHHH  AAAAAA    T \s
-                C       H   H  A    A    T \s
-                 CCCCC  H   H  A    A    T \s""";
-        System.out.println("Hello I'm \n" + logo);
-        System.out.println("What can I do for you?\n");
-        ArrayList<Task> toDoList = new ArrayList<Task>();
-        int size = 0;
-        String input;
-        Scanner in = new Scanner(System.in);
+        ArrayList<Task> taskList = new ArrayList<Task>();
+        Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            input =  in.nextLine();
-            if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n");
-                break;
-            }
-            else if (input.equals("list")) {
-                for (int i = 0; i < toDoList.size(); i++) {
-                    Task currTask = toDoList.get(i);
-                    System.out.println(i + 1 + ". " + currTask);
-                }
-            }
-            else if (input.contains("unmark ")) {
-                String taskDesc = input.substring(input.indexOf("unmark") + 7);
-                for (Task currTask : toDoList) {
-                    if (currTask.description.equals(taskDesc)) {
-                        currTask.markTask(false);
-                        System.out.println("Not nice, I've unmarked this task: ");
-                        System.out.println(currTask);
-                        break;
-                    }
-                }
-            }
-            else if (input.contains("mark ")) {
-                String taskDesc = input.substring(input.indexOf("mark")+5);
-                for (Task currTask : toDoList) {
-                    if (currTask.description.equals(taskDesc)) {
-                        currTask.markTask(true);
-                        System.out.println("Nice, I've marked this task done: ");
-                        System.out.println(currTask);
-                        break;
-                    }
-                }
-            }
-            else {
-                Task newTask = new Task("");
-                String[] parts = input.split(" ", 2);
-                String taskType = parts[0];
-                String taskDetails = parts[1];
+        printWelcome();
 
-                switch (taskType) {
-                case "todo": {
-                    newTask = new Todo(taskDetails);
-                    break;
-                }
-                case "deadline": {
-                    String[] subParts = taskDetails.split(" /by ", 2);
-                    String description = subParts[0];
-                    String by = subParts[1];
-                    newTask = new Deadline(description, by);
-                    break;
-                }
-                case "event": {
-                    String[] subParts = taskDetails.split(" /from ", 2);
-                    String description = subParts[0];
-                    String[] subSubParts = subParts[1].split(" /to ", 2);
-                    String to = subSubParts[0];
-                    String from = subSubParts[1];
-                    newTask = new Event(description, to, from);
-                    break;
-                }
-                }
-
-                toDoList.add(newTask);
-                size ++;
-                System.out.println("Added: " + newTask);
-                System.out.println(String.format("Now you have %d tasks in the list", size));
-            }
-            System.out.println("----------------------------------------");
+        while (isActive) {
+            String input =  scanner.nextLine();
+            handleCommand(input, taskList);
+            System.out.println(DIVIDER);
         }
+
+        scanner.close();
+    }
+
+    private static void printWelcome() {
+        System.out.println("Hello I'm \n" + LOGO);
+        System.out.println("What can I do for you?\n");
+    }
+
+    private static void handleCommand(String input, ArrayList<Task> taskList) {
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!\n");
+            isActive = false;
+        }
+        else if (input.equals("list")) {
+            printList(taskList);
+        }
+        else if (input.startsWith("mark ")) {
+            markOrUnmarkTask();Task(taskList, input.substring(5), true);
+        }
+        else if (input.startsWith("unmark ")) {
+            markOrUnmarkTask(taskList, input.substring(7), false);
+        }
+        else {
+            createAndAddTask(input, taskList);
+        }
+
+    }
+
+    private static void printList(ArrayList<Task> taskList) {
+        if (taskList.isEmpty()) {
+            System.out.println("Your task list is empty.");
+            return;
+        }
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println((i + 1) + ". " + taskList.get(i));
+        }
+    }
+
+    private static Task findTaskByDescription(ArrayList<Task> taskList, String description) {
+        for (Task task : taskList) {
+            if (task.getDescription().equals(description)) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    private static void markOrUnmarkTask(ArrayList<Task> taskList, String description, boolean isMarked) {
+        Task task = findTaskByDescription(taskList, description);
+        if (task == null) {
+            System.out.println("Task not found.");
+            return;
+        }
+
+        task.markTask(isMarked);
+        if (isMarked) {
+            System.out.println("Nice, I've marked this task done:");
+        } else {
+            System.out.println("Not nice, I've unmarked this task:");
+        }
+        System.out.println(task);
+    }
+
+    private static void createAndAddTask(String input, ArrayList<Task> taskList) {
+        String[] inputParts = input.split(" ", 2);
+
+        if (inputParts.length < 2) {
+            System.out.println("Invalid add task format.");
+            return;
+        }
+
+        String taskType = inputParts[0];
+        String taskDetails = inputParts[1];
+
+        Task newTask = null;
+
+        switch (taskType) {
+        case "todo": {
+            newTask = new Todo(taskDetails);
+            break;
+        }
+        case "deadline": {
+            String[] subParts = taskDetails.split(" /by ", 2);
+            String description = subParts[0];
+            String by = subParts[1];
+            newTask = new Deadline(description, by);
+            break;
+        }
+        case "event": {
+            String[] subParts = taskDetails.split(" /from ", 2);
+            String description = subParts[0];
+            String[] subSubParts = subParts[1].split(" /to ", 2);
+            String to = subSubParts[0];
+            String from = subSubParts[1];
+            newTask = new Event(description, to, from);
+            break;
+        }
+        }
+
+        taskList.add(newTask);
+        System.out.println("Added: " + newTask);
+        System.out.printf("Now you have %d tasks in the list%n", taskList.size());
     }
 }
